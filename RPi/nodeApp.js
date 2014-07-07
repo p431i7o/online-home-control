34a486fec32a5d65bf87b2b9358cf6ab884f5636
Pablo Federico Ruiz Diaz Rios <pablo@darkstar.example.net>
2014-07-06 20:55:41 -0400

    testing post and get capabilities

--- RPi/nodeApp.js
+++ RPi/nodeApp.js
@@ -5,18 +5,34 @@ var path = require('path');
 var filePath = path.join(__dirname+'/index.html');
 
 http.createServer(function(req, response){
-fs.readFile(filePath,{encoding:'utf-8'},function(err,data){
-  if(!err){
-//    console.log('datos recibidos' + data);
-    console.log('llamada recibida');
-//    console.log(req);
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.write(data);
-    response.end();
+  if(req.method=='POST'){
+    var body = '';
+    request.on('data', function (data) {
+	body += data;
+
+	// Too much POST data, kill the connection!
+	if (body.length > 1e6)
+	    req.connection.destroy();
+    });
+    request.on('end', function () {
+    var post = qs.parse(body);
+    console.log('POST recibido');
+    console.log(post);
   }else{
-    console.log('err');
-    console.log(err);
-  }
-});
+    
+    fs.readFile(filePath,{encoding:'utf-8'},function(err,data){
+      if(!err){
+    //    console.log('datos recibidos' + data);
+	console.log('llamada recibida');
+    //    console.log(req);
+	response.writeHead(200,{'Content-Type':'text/html'});
+	response.write(data);
+	response.end();
+      }else{
+	console.log('err');
+	console.log(err);
+      }
+    });
+  }//else post
 }).listen(80,'10.1.1.100');
 console.log('Server running at http://10.1.1.100:80');
