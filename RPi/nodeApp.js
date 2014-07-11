6f769aac08f6274092d208ca0f1d8ac0944167b4
Pablo Federico Ruiz Diaz Rios <pablo@darkstar.example.net>
2014-07-11 09:39:31 -0400

    Added light modifications, now the page has a hidden form that sends the message and it received througth POST variables and print them on console, next modifications will allow to execute those commands on the raspberry pi and send them trough the 2.4 transmitters

--- RPi/nodeApp.js
+++ RPi/nodeApp.js
@@ -18,10 +18,26 @@ http.createServer(function(req, response){
     req.on('end', function () {
     	var post = qs.parse(body);
     	console.log('POST recibido');
-    	console.log(post);
+	console.log('messagebin');
+	console.log(post.messagebin);
+    	console.log('messagehex');
+	console.log(post.messagehex);
 	response.writeHead(200,{'Content-Type':'text/html'});
-	response.write("{status:'ok',message:'satisfactoriamente recibido'}");
-	response.end();
+	//response.write("{status:'ok',message:'satisfactoriamente recibido'}");
+	//response.end();
+	fs.readFile(filePath,{encoding:'utf-8'},function(err,data){
+	if(!err){
+      //    console.log('datos recibidos' + data);
+	  console.log('llamada recibida');
+      //    console.log(req);
+	  response.writeHead(200,{'Content-Type':'text/html'});
+	  response.write(data);
+	  response.end();
+	}else{
+	  console.log('err');
+	  console.log(err);
+	}
+      });
     });
   }else{
     
@@ -39,5 +55,5 @@ http.createServer(function(req, response){
       }
     });
   }//else post
-}).listen(80,'10.1.1.100');
-console.log('Server running at http://10.1.1.100:80');
+}).listen(1337,'192.168.1.240');
+console.log('Server running at http://192.168.1.240:1337');
