4608ebcff3e482d2e7e951c310b1956ad68c1043
p431i7o <pablito.federico@gmail.com>
2014-08-10 22:46:31 -0400

    Removing innecesary code, cleaning a little

--- RPi/example.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var express = require('express');
-http.createServer(function(req, res){
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.end('Hola Mundo!\n');
-}).listen(80,'10.1.1.100');
-console.log('Server running at http://127.0.0.1:1337');
--- utilTrash/test.cpp
+++ utilTrash/test.cpp
@@ -1,26 +1,6 @@
 #include <stdio.h>
-//#include <iostream>
 #include <stdlib.h>
-//#include <unistd.h>
-//#include <getopt.h>
-//#include <string.h>
-//#include <string>
-//#include <cstdlib>
-/*
-bool enviarMensaje(char* mensaje){
-	printf("\nMensaje:\n");
-	
-	printf("Mensaje es: %s\n",mensaje);
-	int actualSize=0,i=0;
-	for(;mensaje[i]!='\0';){
-		i++;
-		actualSize++;
-	}
-	printf("\nSizeof %i\n",actualSize);
-	return true;
-}
-*/ 
-int main(/* int argc,char ** argv*/){
+int main(){
 char cadena[] = "0x8808A46";
 char * pEnd;
    unsigned long numero;
@@ -28,21 +8,4 @@ char * pEnd;
    
    printf("numero %s=%ld",cadena,numero);
    return 0;
-   /*	char opcion;
-	int contador=0;
-//	extern char *optarg;
-	while((opcion = getopt(argc,argv,"f:"))!=-1){
-		if(opcion=='f'){
-	            printf("\nEnviando comando!\n");
-		    enviarMensaje(optarg);
-            	}else{	
-			printf("\n\r====Ayuda=======\n");
- 		        printf("\n\rUsar la opcion -f  ");
-	                printf("\nCodigo hex para enviar.");
-			printf("\n\rPor ejemplo: ");
-			printf("\nsudo ./rpi-transmiter -f 0x880001AA\n");
-		}
-		if(contador<5){ return 0; } else { return 2;}
-	}
-*/
 }
